324b779f0cbcf12d38dac9520d970056b50b0069
nishiaya <a.nishimura.000@gmail.com>
2020-11-05 23:01:23 +0900

    第５章

---
+++ src/main/scala/practice/Step5.scala
@@ -0,0 +1,15 @@
+package practice
+
+object Step5 extends App {
+
+  //加工文字列リテラル
+  val name = "Tom"
+  println(s"Hello, $name!")
+
+  //算術演算
+  println('b' - 'a') // 1
+
+  println(List(1,2,3) == List(1,2,3))
+
+  println(1 == 1.0)
+}
